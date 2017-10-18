a786f6175e494ed884ceaef92be44480d07685ff
Huitse Tai <geb.1989@gmail.com>
2017-10-18 12:50:33 +0800

    patch IsDotFile function compatible with both '.' and '..'
    Signed-off-by: Huitse Tai <geb.1989@gmail.com>

--- utils.go
+++ utils.go
@@ -40,8 +40,9 @@ func IsConfiguration(path string) bool {
 
 // IsDotFile returns whether or not path has dot as a prefix.
 func IsDotFile(path string) bool {
+	path = filepath.Clean(path)
 	base := filepath.Base(path)
-	return strings.HasPrefix(base, ".") && len(base) > len(".")
+	return strings.HasPrefix(base, ".") && base != "." && base != ".."
 }
 
 // IsVendor returns whether or not path is a vendor path.
