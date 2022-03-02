fe6167a3aac8c522f38f386f9ed8235656afc289
UtsavChokshi <utsav.chokshi@codenation.co.in>
2022-03-02 10:28:43 +0530

    Expose GetLanguageType

--- java/src/main/java/tech/sourced/enry/Enry.java
+++ java/src/main/java/tech/sourced/enry/Enry.java
@@ -247,4 +247,14 @@ public class Enry {
     public static synchronized boolean isTest(String path) {
         return toJavaBool(nativeLib.IsTest(toGoString(path)));
     }
+
+    /**
+     * Returns type for given language.
+     *
+     * @param language of the file
+     * @return type
+     */
+    public static synchronized String getLanguageType(String language) {
+        return toJavaString(nativeLib.GetLanguageType(toGoString(language)));
+    }
 }
